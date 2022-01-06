21f796a597ff1660a092fea914bbbda4c6532f9e
Leonardo Faria <leonardofaria00@gmail.com>
2022-01-06 12:26:44 -0300

    First commit

---
+++ src/main/java/br/com/domain/application/ApiHandlerInterceptorApplication.java
@@ -0,0 +1,13 @@
+package br.com.domain.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHandlerInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHandlerInterceptorApplication.class, args);
+	}
+
+}
